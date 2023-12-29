d2da4e21a3079c149f5505fe6aabe67d591e7955
Christophe Schutz <schutz.christophe@gmail.com>
2023-12-29 17:27:56 +0100

    feat(MovieRental): Resolve MovieRental kata

--- src/main/java/com/hypesofts/kata/movierental/Rental.java
+++ src/main/java/com/hypesofts/kata/movierental/Rental.java
@@ -5,19 +5,44 @@ package com.hypesofts.kata.movierental;
  */
 public class Rental {
 
-    private Movie _movie;
-    private int _daysRented;
+    private Movie movie;
+    private int daysRented;
 
     public Rental(Movie movie, int daysRented) {
-        _movie = movie;
-        _daysRented = daysRented;
+        this.movie = movie;
+        this.daysRented = daysRented;
     }
 
     public int getDaysRented() {
-        return _daysRented;
+        return daysRented;
     }
 
     public Movie getMovie() {
-        return _movie;
+        return movie;
+    }
+
+    public double getAmount() {
+        //determine amounts for each line
+        double result = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
+
+    public boolean isFrequentRenterBonusApplicable() {
+        return (movie.getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1;
     }
 }
---
+++ src/main/java/com/hypesofts/kata/movierental/Statement.java
@@ -0,0 +1,24 @@
+package com.hypesofts.kata.movierental;
+
+import java.util.List;
+
+public interface Statement {
+
+    default String print(Customer customer) {
+        String result = printHeader(customer.getName());
+        result += printRentals(customer.getRentalSummary().getRentals());
+        result += printAmountOwned(customer.getRentalSummary().getTotalAmount());
+        result += printFrequentRenterPoints(customer.getRentalSummary().getTotalFrequentRenterPoints());
+
+        return result;
+    }
+
+    String printFrequentRenterPoints(int totalFrequentRenterPoints);
+
+    String printAmountOwned(double totalAmount);
+
+    String printRentals(List<Rental> rentals);
+
+    String printHeader(String name);
+
+}
