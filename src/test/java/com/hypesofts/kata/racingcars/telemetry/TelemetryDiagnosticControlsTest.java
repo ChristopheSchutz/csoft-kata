69555fe0ff8314911b5c4651bcf6d017b390aac2
Christophe Schutz <schutz.christophe@gmail.com>
2023-12-29 15:21:41 +0100

    feat(RacingCars): Add Telemetry kata

---
+++ src/test/java/com/hypesofts/kata/racingcars/telemetry/TelemetryDiagnosticControlsTest.java
@@ -0,0 +1,13 @@
+package com.hypesofts.kata.racingcars.telemetry;
+
+import org.junit.jupiter.api.Test;
+
+public class TelemetryDiagnosticControlsTest
+{
+    
+	@Test
+    public void CheckTransmission_should_send_a_diagnostic_message_and_receive_a_status_message_response()
+    {
+    }
+
+}
