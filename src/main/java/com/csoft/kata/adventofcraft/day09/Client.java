9e3fc6e226f9caba290e0f6ca657d6e7bc267a09
Christophe Schutz <schutz.christophe@gmail.com>
2024-02-02 14:37:19 +0100

    feat(AdventOfCraft): Resolve Day 09

---
+++ src/main/java/com/csoft/kata/adventofcraft/day09/Client.java
@@ -0,0 +1,29 @@
+package com.csoft.kata.adventofcraft.day09;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Client {
+    private final Map<String, Double> orderLines;
+    private double totalAmount;
+
+    public Client(Map<String, Double> orderLines) {
+        this.orderLines = orderLines;
+        this.totalAmount = orderLines.values().stream().mapToDouble(Double::doubleValue).sum();
+    }
+
+    public String toStatement() {
+        return orderLines.entrySet().stream()
+                .map(entry -> formatLine(entry.getKey(), entry.getValue()))
+                .collect(Collectors.joining(System.lineSeparator()))
+                .concat(System.lineSeparator() + "Total : " + totalAmount + "€");
+    }
+
+    private String formatLine(String name, Double value) {
+        return name + " for " + value + "€";
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+}
